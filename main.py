47d95eccbff6301318067c85467dfc74c72f0a8f
arijitnirmal1 <arijit.nirmal1@gmail.com>
2026-05-20 20:28:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv("data.csv")
+x=df['HoursStudied']
+y=df['ExamScore']
+x_train,x_test,y_train,y_test
 st.title("My First Streamlit App")
 st.write("Hello! Creating a simple web application using Streamlit.")
 name=st.text_input("Enter your name:")
