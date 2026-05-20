ed84d7283d3f3d957da4683d7206c149633e3856
arijitnirmal1 <arijit.nirmal1@gmail.com>
2026-05-20 19:45:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("My First Streamlit App")
 st.write("Hello! Creating a simple web application using Streamlit.")
-st.text_input("Enter your name:")
+name=st.text_input("Enter your name:")
 #Displaying a message when a button is clicked
 if st.button("Submit"):
   st.write(f"Hello, {name}! Welcome to Streamlit.")
