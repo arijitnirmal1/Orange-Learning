7611e2aa27ae0f9c7c8f82258f1e0c016c92cf4e
arijitnirmal1 <arijit.nirmal1@gmail.com>
2026-05-20 21:23:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ y=df['ExamScore']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 model=LinearRegression()
 model.fit(X_train,y_train)
-st.title("chr(128217)Exam Score Predictor")
+st.title("Exam Score Predictor")
 st.write("Enter hours studied to predict the exam score.")
 hours=st.number_input("Hours Studied:",min_value=0.0,step=0.1)
 #Displaying a message when a button is clicked
 if st.button("Predict Score"):
   predicted_score=model.predict([[hours]])[0]
-  st.success(f"chr(9745) Predicted Score: {predicted_score:.2f}")
-st.write("###chr(128202) Sample Training Data")
+  st.success(f"Predicted Score: {predicted_score:.2f}")
+st.write("###Sample Training Data")
 st.dataframe(df)
